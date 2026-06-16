e60db84bb9a3abfb523e30f3f3e046810cf5acf3
Raghavan <raghavan.mit@gmail.com>
2026-06-15 22:32:06 -0700

    Solve sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -5,9 +5,9 @@ def sigmoid(x):
     Vectorized sigmoid function.
     """
     x = np.asarray(x, dtype=np.float64)
-    pos = x > 0
+    pos = x >= 0
     z = np.empty_like(x)
     z[pos] = 1.0 / (1.0 + np.exp(-x[pos]))
-    exp_x = np.exp(x[~pos])
-    z[~pos] = exp_x / (1.0 + exp_x)
+    ex = np.exp(x[~pos])
+    z[~pos] = ex / (1.0 + ex)
     return z
