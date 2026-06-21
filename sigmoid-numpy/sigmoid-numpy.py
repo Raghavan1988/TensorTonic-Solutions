5fd5ae4e84a4ec1a92092a9da32e72181f9dc077
Raghavan <raghavan.mit@gmail.com>
2026-06-21 13:45:57 -0700

    Solve sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -4,10 +4,15 @@ def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
-    x = np.asarray(x, dtype=np.float64)
-    pos = x >= 0
-    z = np.empty_like(x)
-    z[pos] = 1.0 / (1.0 + np.exp(-x[pos]))
-    ex = np.exp(x[~pos])
-    z[~pos] = ex / (1.0 + ex)
+    
+    X = np.asarray(x, dtype=np.float64) # 
+    pos = X >= 0 ## creates a masked array
+    neg = X < 0 ## creates 
+    
+    z = np.empty_like(X) ## intitializing the tensor shapes
+    #catch (do the calculation separately for positive values and negative values)
+    z[pos] = 1.0 / (1.0 + np.exp(-X[pos]))
+    ## if the input itself is negative ( e^x / (1 + e^x))
+    Y = np.exp(X[neg])
+    z[neg] = Y / (1.0 + Y)
     return z
